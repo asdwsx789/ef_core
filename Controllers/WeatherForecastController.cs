001c6746016aee6a0d4af8d4cf59fbb6d7e65677
TW-1130-黃裕善 <u10622@umec.com.tw>
2024-06-21 13:50:23 +0800

    create hello world api.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -29,4 +29,10 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+
+    [HttpGet("GetString")]
+    public IActionResult GetString()
+    {
+        return Ok("hello world");
+    }
 }
